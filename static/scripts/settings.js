f93b320fc39444cf7a580164be121cfc3a5693f0
Saranthyr <kirillmaksimets@gmail.com>
2022-07-07 08:05:30 +0300

    [+]Project finished

--- static/scripts/settings.js
+++ static/scripts/settings.js
@@ -1,24 +1,19 @@
-document.getElementById('button').addEventListener('click', async function sd() {
-        var op = document.getElementById('old_pwd').value;
-        var np = document.getElementById('new_pwd').value;
-        var npr = document.getElementById('new_pwd_rep').value;
-        let fd = new FormData();
-        fd.append("old_pwd", op);
-        fd.append("new_pwd", np);
-        fd.append("new_pwd_repeat", npr);
-        let response = await fetch('/{{ username }}/settings', {method: 'POST', body: fd});
-        if (response.status == 265) {
-            alert('Current password does not match')
-        }
-        if (response.status == 255) {
-            alert('New password does not match')
-        }
+window.onload = function () {
+    document.getElementById('button').addEventListener('click', async function sd() {
+
+            var op = document.getElementById('old_pwd').value;
+            var np = document.getElementById('new_pwd').value;
+            var npr = document.getElementById('new_pwd_rep').value;
+            let fd = new FormData();
+            fd.append("old_pwd", op);
+            fd.append("new_pwd", np);
+            fd.append("new_pwd_repeat", npr);
+            let response = await fetch('/{{ username }}/settings', {method: 'POST', body: fd});
+            if (response.status == 265) {
+                alert('Current password does not match')
+            }
+            if (response.status == 255) {
+                alert('New password does not match')
+            }
     });
-    document.getElementById('logout').addEventListener('click', async function sd() {
-        let response = await fetch('/logout', {method: 'POST', body: ''});
-        if (response.status == 302) {
-            var url = await response.json();
-            var url = url['url'];
-            window.location.replace(url);
-        };
-    })
+}
